f0b283d4f2fdfe387197e4a9e4d84d6debf71185
Jonathan Gnagy <jgnagy@knuedge.com>
2016-11-23 14:25:01 -0800

    Adding support for bind-mounting mysql Also added the ability to specify an image tag

--- manifests/management.pp
+++ manifests/management.pp
@@ -2,11 +2,26 @@
 class rancher::management(
   $rancher_manager_port     = '8080',
   $docker_cleanup_container = true,
-  $docker_cleanup_volume    = false
+  $docker_cleanup_volume    = false,
+  $docker_image_tag         = false,
+  $mysql_bind_mount         = false,
+  $mysql_bind_mount_path    = '/usr/local/var/lib/mysql'
 ){
   require docker
 
-  docker::image { 'rancher/server':
+  if $docker_image_tag {
+    docker::image { 'rancher/server':
+      image_tag => $docker_image_tag
+    }
+  }
+  else {
+    docker::image { 'rancher/server':
+    }
+  }
+
+  $volumes = $mysql_bind_mount ? {
+    true    => ["${mysql_bind_mount_path}:/var/lib/mysql"],
+    default => []
   }
 
   docker::run { 'rancher/server':
@@ -17,5 +32,6 @@ class rancher::management(
     remove_volume_on_start    => $docker_cleanup_volume,
     remove_container_on_stop  => $docker_cleanup_container,
     remove_volume_on_stop     => $docker_cleanup_volume,
+    volumes                   => $volumes
   }
 }
