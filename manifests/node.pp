e2274065332564c9ecfe6adb03ce3a8f239d6b2b
Jonathan Gnagy <jgnagy@knuedge.com>
2017-04-28 15:30:44 -0700

    Stealing a better bootstrap

--- manifests/node.pp
+++ manifests/node.pp
@@ -2,6 +2,9 @@
 class rancher::node (
   $management,
   $registration_token,
+  $agent_address = $::ipaddress,
+  $docker_socket = '/var/run/docker.sock',
+  $image_tag = 'latest',
   $rancher_master_port = 8080,
   $docker_env = []
 ) {
@@ -10,19 +13,11 @@ class rancher::node (
 
   require docker
 
-  docker::image { 'rancher/agent':
-  }
-
-  docker::run { 'rancher/node':
-    image      => 'rancher/agent',
-    restart    => 'no',
-    privileged => true,
-    command    => "http://${management}:${rancher_master_port}/v1/scripts/${registration_token}",
-    volumes    => [
-      '/var/run/docker.sock:/var/run/docker.sock',
-      '/var/lib/rancher:/var/lib/rancher'
-    ],
-    require    => Docker::Image['rancher/agent'],
-    env        => $docker_env
+  docker::image { 'rancher/agent': } ->
+  exec { 'bootstrap rancher agent':
+    path      => ['/usr/local/bin', '/usr/bin', '/bin'],
+    logoutput => true,
+    command   => "docker run --privileged -v ${docker_socket}:/var/run/docker.sock -v /var/lib/rancher:/var/lib/rancher -e 'CATTLE_AGENT_IP=${agent_address}' rancher/agent:${image_tag} http://${management}:${rancher_master_port}/v1/scripts/${registration_token}",
+    unless    => 'docker inspect rancher-agent',
   }
 }
